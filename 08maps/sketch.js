c92cc3fabc55050f6220e907e00f38bbd2a2909c
Kaush <kaush@kmac.local>
2015-02-27 01:09:15 -0500

    Code cleanup sketch.js

--- 08maps/sketch.js
+++ 08maps/sketch.js
@@ -1,34 +1,10 @@
 try {
 
     try {
-        /* global L:false,
-          featureCollection:true,
-          mapdiv:false,
-          procoding:false
-    */
-        /*jshint unused:false */
-
-        // initial location and zoom level
-        //var lat = 12.976981;
-        //var lon =  77.573050;
-        //var lon = -75.78281950950623;
-        //var lat = 43.00101433142999;
+        
         var lon = 80.22525787353516;
         var lat = 13.082153590400935;
         var zoom = 12;
-
-        //coordURL = "";
-        //function getCoords() {
-        //				var xhr = new XMLHttpRequest();
-        //				xhr.open("GET", coordURL, true);
-        //				xhr.onreadystatechange = function() {
-        //					if (xhr.readyState == XMLHttpRequest.DONE) {
-        //						var featureCollection = JSON.parse(xhr.responseText);
-        //					}
-        //				};
-        //				xhr.send();
-        //			}
-
 
         // init leflet map
         var map = L.map("mapdiv", {
@@ -66,13 +42,6 @@ try {
                 timeout: 5000
             });
         }
-
-        // Loading Features 
-        // add markers from geoJSON
-        //function loadFeatures() {
-        //    var featureLayer = L.geoJson().addTo(map);
-        //    featureLayer.addData(featureCollection));;
-        //}
 
         // Get coordinates and popup
         var popup = L.popup();
@@ -119,25 +88,6 @@ try {
         };
 
 
-        // L.marker([lat+0.001, lon], {icon: redIcon}).addTo(map).bindPopup("I am a red leaf.");
-
-        // Color Flags
-        /*
-icon: function color(feature){
-    switch(feature.properties.status){
-        case 'red': return 'red';
-        default: return 'yellow';
-    }
-}
-
-{icon: feature.properties.status}
-*/
-
-        //---
-
-
-        //----
-
         var myLayer = L.geoJson(featureCollection, {
             onEachFeature: function display(feature, layer) {
                 //    console.log(feature.properties.status);
@@ -160,29 +110,7 @@ icon: function color(feature){
         function trigger_register_event(id) {
             //console.log("Host Cleaning session for location ID - " + id);
             prompt("Host Cleaning session for location ID - " + id, "");
-
-            //            $(function() {
-            //                $("a" + id).datepicker();
-            //            });
-            //            $("a" + id).click();
-            //document.getElementByID('mapdiv').style.display = 'block';
         }
-
-        //setTimeout(onEachFeature, 200);
-				
-        //   L.geoJson(featureCollection, onEachFeature : function (feature, layer) {
-        //       layer.bindPopup(feature.properties.desc);
-        //   }
-
-        //    L.geoJson(featureCollection, {
-        //        onEachFeature: onEachFeature 
-        //    }).addTo(map);
-        //    
-        //    function onEachFeature(feature, layer) {
-        //        if (feature.properties.name) {
-        //            layer.bindPopup("<a id=a"+feature.properties.id+" href='javascript:void(0);' onclick='trigger_register_event(" + feature.properties.id + ");'>" + feature.properties.name + "</a><br>" + feature.properties.desc);
-        //        }
-        //    }
 
 
     } catch (e) {
